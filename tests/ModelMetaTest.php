2182f7bc8dbddf49c96d2ad8841c5b2df4f206a7
Freek Van der Herten <freek@spatie.be>
2022-08-09 13:49:57 +0200

    wip

--- tests/ModelMetaTest.php
+++ tests/ModelMetaTest.php
@@ -6,5 +6,9 @@ use Spatie\ModelMeta\Tests\TestSupport\Models\RelationTestModel;
 it('can get meta information about a model', function () {
     $modelMeta = ModelMeta::forModel(RelationTestModel::class);
 
-    $this->assertMatchesSnapshot($modelMeta->toArray());
+    $modelMeta = $modelMeta->toArray();
+
+    $modelMeta['fileName'] = str_replace($this->getTestDirectory(), '', $modelMeta['fileName']);
+
+    $this->assertMatchesSnapshot($modelMeta);
 });
