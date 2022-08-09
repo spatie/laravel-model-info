790ec808f4bc1864d023b93c2ec4782d6286795e
Freek Van der Herten <freek@spatie.be>
2022-08-09 17:45:03 +0200

    wip

--- src/Relations/RelationFinder.php
+++ src/Relations/RelationFinder.php
@@ -28,7 +28,7 @@ class RelationFinder
     ];
 
     /**
-     * @param  lass-string<Model>|Model  $model
+     * @param class-string<Model>|Model $model
      * @return Collection<Attribute>
      */
     public static function forModel(string|Model $model): Collection
